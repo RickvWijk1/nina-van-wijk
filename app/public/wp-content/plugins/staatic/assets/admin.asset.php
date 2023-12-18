909e16e14efa8107b2dd85cb73c2f4ee5494958a
rvanwijk <rickvanwijk@bluebirdday.nl>
2023-12-18 15:27:12 +0100

    new wordpress site commit

--- app/public/wp-content/plugins/staatic/assets/admin.asset.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Staatic\Vendor;
-
-return array(
-    'dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'),
-    'version' => 'b7bf102b61e8515768b9'
-);
